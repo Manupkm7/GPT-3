8f6c86f3687710fa12f89efa49598b22754c80dc
Manupkm7 <manuelespinoza238@gmail.com>
2022-06-08 14:53:36 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
