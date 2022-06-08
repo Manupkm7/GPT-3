5787a3e33886586a9339404ddf4907a64b1a8729
Manupkm7 <manuelespinoza238@gmail.com>
2022-06-08 16:22:00 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./components/Containers";
 import { Brand, Navbar } from "./components/Sections/index";
-import CTA from "./components/Sections/Cta/Cta";
+import CTA from "./components/Sections/Cta/Cta.jsx";
 import "./App.css";
 
 const App = () => (
